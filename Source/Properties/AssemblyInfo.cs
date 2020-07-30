62b248ab55caae3dfee0b14c50e55858bf1c7919
Andreas Pardeike <andreas@pardeike.net>
2020-07-30 21:16:50 +0200

    v1.0.2 for RimWorld 1.2

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -9,8 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
+
 [assembly: Guid("a9ece402-1da1-42b2-b8f5-33856986cff6")]
-[assembly: AssemblyVersion("1.0.1.1")]
-[assembly: AssemblyFileVersion("1.0.1.1")]
+
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
